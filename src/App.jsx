c121ad33444c9140e0497fc47bb3c8e26d4e3a93
faizanKhan <faizankhan46849@gmail.com>
2024-12-12 23:37:24 +0530

    starting

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className=" bg-red-500 text-white flex justify-center font-bold text-3xl p-2">Faizan-shify</h1>
+    </>
+  )
+}
+
+export default App
